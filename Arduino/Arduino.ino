981ad571eae135b29f88733c13abf8b6c955cb30
Alvaro Gabriel Gonzalez Martinez <aggm123@icloud.com>
2021-06-02 15:42:32 -0700

    PID Integration.

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,7 +1,7 @@
 //--------//====================================//--------//
 // A duplex comunication between Arduino and Raspberry py //
 // Autor: Alvaro Gabriel Gonzalez Martinez                //
-// Version 2.0 - Arduino 27/5/2021                        //
+// Version 2.0 - Arduino 2/6/2021                        //
 //--------//====================================//--------//
 // Requirenments
 #include <stdio.h>      /* printf, fgets */
@@ -17,6 +17,12 @@ float Data[Ctotal]; /* Our Data inicialized by Ctotal.    */
 const int Su[3][2] = {{9, 10}, {12, 11}};
 /* |==|         Motors        |==| */
 const int Mo[2][3] = {{8, 7, 6}, {3, 4, 5}};
+/* |==|   PID variables  |==| */
+int maximumRange = 200; // maximo rango
+int minimumRange = 0; // Minimo rango
+float duration, distance, actual, sp = 20, error;
+float previous_error, drive, derivative = 0, integral = 0, kp = 0.28;
+float kd = 0.1, ki = 0.00001, salida, x, bandera = 0;
 
 /* [INIT] */ /* |==| Arduino configuration |==| */
 void setup() {
@@ -110,6 +116,7 @@ void motorsOff() {
 /* Parameters */
 /* MotorDirecction(leftDirection, rightDirecction) using max power or     */
 /* MotorDirecction(leftDirection, rightDirecction, leftPower, rigthPower) */
+/* MotorDirecction(leftDirection, rightDirecction, Power) */
 void motorDirecction(int I, int D) {
   motorsOff();
   digitalWrite(Mo[0][I], 1);
@@ -124,6 +131,13 @@ void motorDirecction(int I, int D, int Ipow, int Dpow) {
   analogWrite(Mo[0][2], map(Ipow, 0, 100, 100, 255));
   analogWrite(Mo[1][2], map(Dpow, 0, 100, 100, 255));
 }
+void motorDirecction(int I, int D, int Pow) {
+  motorsOff();
+  digitalWrite(Mo[0][I], 1);
+  digitalWrite(Mo[1][D], 1);
+  analogWrite(Mo[0][2], map(Pow, 0, 100, 100, 255));
+  analogWrite(Mo[1][2], map(Pow, 0, 100, 100, 255));
+}
 void antiCollision() {
   if (Distancia(0) < 20)
     motorDirecction(1, 0);
@@ -132,7 +146,7 @@ void antiCollision() {
   else
     motorDirecction(1, 1, 70, 70);
 }
-/*  */
+/* Batt state */
 void battState() {
   // The intensity of the led depends on the load
   analogWrite(battLED, map(Data[2], 0, 110, 0, 200));
@@ -143,6 +157,45 @@ void battState() {
     Data[Cdatos + 1] = 30 + (100 - Data[2]) * 2;
   else
     Data[Cdatos + 1] = 100;
+}
+void PID_0(float distance) {
+  if (distance >= maximumRange || distance <= minimumRange) {
+    /* "Out of range" signal */
+    Serial.println("-1");
+  }
+  else {
+    if (drive > -5 && drive < 5)
+    {
+      error = sp - actual;
+      integral = integral + (error);  // Integral value
+      derivative = (error - previous_error); // Derivative value
+      drive = (error * kp) + (integral * ki) + (derivative * kd); // PID calculation
+      previous_error = error;
+    } else
+    {
+      error = sp - actual;
+      drive = (error * kp) + (integral * ki) + (derivative * kd);
+      previous_error = error;
+    }
+    if (drive > 5)
+    {
+      drive  = 5; // 5 as our range value to drive the PWM
+    }
+    else if (drive < -5)
+    {
+      drive = -5;
+    }
+    if (drive < 0)
+    {
+      motorDirecction(1, 1, map(drive, -5, 0, 90, 0));
+    }
+    else
+    {
+      motorDirecction(0, 0, map(drive, -5, 0, 90, 0));
+    }
+    actual = distance;
+  }
+  delay(10);
 }
 //--------/  Loop  /--------//
 void loop() {
@@ -156,11 +209,20 @@ void loop() {
     case 1:
       antiCollision();
       break;
+    case 2:
+      PID_0(Data[1]);
+      break;
+    case 3:
+      for (int i = 0; i < 20; i++)
+        motorDirecction(1, 1, 100);
+      for (int i = 0; i < 10; i++)
+        PID_0(Data[0]);
+      break;
     case 5:
-      motorDirecction(0, 0, Data[Cdatos + 1 ] + 2, Data[Cdatos + 1] + 2);
+      motorDirecction(0, 0, 100);
       break;
     case 8:
-      motorDirecction(1, 1, Data[Cdatos + 1], Data[Cdatos + 1]);
+      motorDirecction(1, 1, 100);
       break;
     case 4:
       motorDirecction(0, 1, Data[Cdatos + 1], Data[Cdatos + 1]);
